005585abb399e136b9053a3e384bbbc095b8fa68
Matt <philmattdev@gmail.com>
2023-09-14 15:45:36 +0800

    feat: add models and types related to OpenAI

--- library/models.py
+++ library/models.py
@@ -1,5 +1,19 @@
 from pydantic import BaseModel
 from enum import Enum
+from typing import TypedDict, Optional
+from library.types import OpenAIRoleType
+
+
+class OpenAIFunctionCall(TypedDict):
+    name: str
+    arguments: str
+
+
+class OpenAIMessage(TypedDict):
+    role: OpenAIRoleType
+    content: str
+    name: Optional[str]
+    function_call: Optional[OpenAIFunctionCall]
 
 
 class SeasonEnum(Enum):
---
+++ library/types.py
@@ -0,0 +1,3 @@
+from typing import Literal
+
+OpenAIRoleType = Literal["system", "user", "assistant", "function"]
