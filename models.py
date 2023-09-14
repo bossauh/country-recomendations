11b43ce2d77e168552093be5accb0b3ca7e7c1be
Matt <philmattdev@gmail.com>
2023-09-14 15:29:32 +0800

    feat: add models

--- models.py
+++ models.py
@@ -1,1 +1,15 @@
 from pydantic import BaseModel
+from enum import Enum
+
+
+class SeasonEnum(Enum):
+    SPRING = "spring"
+    SUMMER = "summer"
+    FALL = "fall"
+    WINTER = "winter"
+
+
+class RecommendResponse(BaseModel):
+    country: str
+    season: str
+    recommendations: list[str]
