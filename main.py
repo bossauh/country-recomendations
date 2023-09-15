ea4589721112831e18a19e0258092d33fdfa9a54
Matt <philmattdev@gmail.com>
2023-09-15 19:36:33 +0800

    feat: allow requests from any origin

--- main.py
+++ main.py
@@ -3,11 +3,15 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from library.models import SeasonEnum, RecommendResponse
 from library.engine import recommender
 
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware, allow_origins=["*"], allow_methods=["*"], allow_headers=["*"]
+)
 
 
 @app.get(
